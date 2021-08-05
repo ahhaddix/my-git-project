1592fd3c0c9c4ff978a913a2c4846bae3794ac86
ahhaddix <amyhaddix@rocketmail.com>
2021-08-05 16:45:17 -0400

    finished saving conflicts

--- my_file.py
+++ my_file.py
@@ -6,8 +6,5 @@ def hi_there():
     print("hi")
 
     def hi_there():
-<<<<<<< HEAD
-    print("My local change")
-=======
     print("Update python function with merge conflict")
->>>>>>> origin/master
+
